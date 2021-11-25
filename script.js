fb46c73723b67cbc4c5fbd9974f9a438758a64ee
Gregor <gregorlthomson@sky.com>
2021-11-25 12:08:27 +0000

    Write function to render one element

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log(dataObject);
+const availablePets = document.querySelector(".avail-pets");
+const homedPets = document.querySelector(".homed-pets");
+
+// Function to add an element to the page
+
+const renderOneElement = (dataEntry, parent) => {
+  let article = document.createElement("article");
+
+  let title = document.createElement("h3");
+  let titleText = document.createTextNode(dataEntry.name);
+  title.appendChild(titleText);
+  article.appendChild(title);
+
+  let img = document.createElement("img");
+  // Check type of dataEntry, assign image src and alt
+  let animalType = dataEntry.type;
+  img.src = `/assets/${animalType}.jpg`;
+  img.alt = `An image of a ${animalType}`;
+  article.appendChild(img);
+
+  const nature = document.createElement("p");
+  const natureText = document.createTextNode(dataEntry.nature);
+  nature.appendChild(natureText);
+  article.appendChild(nature);
+
+  parent.appendChild(article);
+};
+
+// Function to check the status, pass parent to renderOneElement
+
+// Function to render all data, Iterate and run RenderOneElement for all
