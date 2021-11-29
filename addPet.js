d3323c96b8dbea091b30e7542963d5a02172e6c1
Gregor <gregorlthomson@sky.com>
2021-11-29 15:29:47 +0000

    Add steps for workshop

---
+++ addPet.js
@@ -0,0 +1,9 @@
+// Add an HTML Form to the page
+// This should allow a user to add a new pet to the adoption agency
+
+// Grab the elements from the page
+
+// Add the pet to the object
+// Consider how the data should be handled and passed to the object
+
+// Re-render the page whenever a new pet is added
